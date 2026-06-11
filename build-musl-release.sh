6dac8fc9e8dff7e0eb3858770f19a1863bb50944
Vladislav Minakov <v@minakov.pro>
2026-06-11 22:20:07 +0300

    build: add aivpn-ios-core to musl cross-build Docker context
    The workspace Cargo.toml declares aivpn-ios-core as a member; without
    copying it into the Docker build context the workspace resolution fails
    with "failed to read aivpn-ios-core/Cargo.toml".

--- build-musl-release.sh
+++ build-musl-release.sh
@@ -110,6 +110,7 @@ COPY aivpn-common aivpn-common/
 COPY aivpn-server aivpn-server/
 COPY aivpn-client aivpn-client/
 COPY aivpn-android-core aivpn-android-core/
+COPY aivpn-ios-core aivpn-ios-core/
 COPY aivpn-windows aivpn-windows/
 COPY aivpn-ios-core aivpn-ios-core/
 COPY aivpn-common/mask-assets mask-assets/
